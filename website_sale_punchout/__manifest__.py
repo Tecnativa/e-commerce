e656d159099dadda6c544b8cfc93c561cf48fab4
David <david.vidal@tecnativa.com>
2023-02-14 18:29:12 +0100

    [ADD] website_sale_punchout: New module
    TT39651

---
+++ website_sale_punchout/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2023 Tecnativa - David Vidal
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Punchot Catalog",
+    "summary": "Allows to generate a Punchout Catalog to upload to Ariba NetWork"
+    " categories",
+    "version": "13.0.1.0.0",
+    "category": "Product",
+    "website": "https://github.com/OCA/e-commerce",
+    "maintainers": ["chienandalu"],
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": ["views/punchout_index_catalog_templates.xml"],
+}
---
+++ website_sale_punchout/readme/CONFIGURE.rst
@@ -0,0 +1,5 @@
+#. Go to ...
+
+.. figure:: ../static/description/image.png
+   :alt: alternative description
+   :width: 600 px
